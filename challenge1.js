03a24c0a903677cf63bad0334839cf6a790c1b5e
Rawanss7 <rawansss@outlook.com>
2022-03-20 18:32:14 +0300

    MODIFIED challenge1.js and challenge2.js

--- challenge1.js
+++ challenge1.js
@@ -3,6 +3,10 @@
  * Create a function named `printName`
  * - that just prints your name on the screen
  */
+ function printName (){
+    console.log("my name is rawan");
+}
+printName();
 
 /**
  * Task 2:
@@ -11,7 +15,11 @@
  * - and prints the age on the screen.
  * - Age = current year - birth
  */
-
+ function printAge(currentYear, birth){
+    return currentYear-birth
+}
+const age = printAge(2022-1992);
+console.log(age)
 /**
  * Task 3:
  * Create a function named `printHello`
@@ -22,10 +30,25 @@
  * -- fr: it should print `Bonjour NAME`
  * -- tr: it should print `Merhaba NAME`
  */
-
+ if (language === "en"){
+    console.log("Hello",name)
+} else if (language === "es"){
+   console.log("Hola",name)
+} else if (language === "fr"){
+   console.log("Bonjour ",name)
+} else if (language === "tr"){
+   console.log("Merhaba ",name)
+}
+console.log(printHello("Eman ","fr"));
 /**
  * Task 4:
  * Create a function named `printMax`
  * - that takes 2 parameters as numbers
  * - should print out the bigger number
  */
+ function printMax(y,x){
+    if (x>y)
+        console.log(x);
+    else
+        console.log(y);
+}
--- challenge2.js
+++ challenge2.js
@@ -7,8 +7,9 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 function greet(name) {
-  // Your code here
+  console.log(`Hello ${name}`);
 }
+greet("Rwan");
 
 /**
  * isOdd(n):
@@ -20,7 +21,9 @@ function greet(name) {
  * isOdd(10) -> false
  */
 function isOdd(n) {
-  // Your code here
+  return n % 2 === 1;
+
+
 }
 
 /**
@@ -33,7 +36,7 @@ function isOdd(n) {
  * oddsSmallerThan(15) -> 7
  */
 function oddsSmallerThan(n) {
-  // Your code here
+  return Math.floor(n / 2);
 }
 
 /**
@@ -47,7 +50,7 @@ function oddsSmallerThan(n) {
  * squareOrDouble(9) -> 81
  */
 function squareOrDouble(n) {
-  // Your code here
+  return isOdd(n) ? n ** 2 : n * 2;
 }
 
 module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
